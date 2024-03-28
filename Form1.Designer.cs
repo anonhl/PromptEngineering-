ac6290d605b2de7b706dc2badcb5eb3f03c3419c
anonhl <50787621+anonhl@users.noreply.github.com>
2024-03-28 21:24:00 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace prompt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(2, 1);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(605, 415);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(540, 491);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "添加";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(540, 454);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(66, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "清空";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(2, 491);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(77, 36);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "打开文档";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(204, 446);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "关键词";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 478);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "中文/内容";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(192, 509);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "英文/备注";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(251, 441);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(280, 21);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(251, 473);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(280, 21);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(251, 506);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(280, 21);
+            this.textBox3.TabIndex = 9;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 428);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "编辑模式";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(98, 428);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(72, 16);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "是否中文";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(85, 446);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(77, 39);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "刷新";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(85, 491);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(77, 37);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "打开文件夹";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(2, 446);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(77, 39);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "新建文件夹";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 529);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PromptEngineering工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; // 提供用于处理文件和目录的类型
+using System.Diagnostics;
+
+
+
+namespace prompt
+{
+    public partial class Form1 : Form
+    {
+        private string path1; // 用于保存路径1
+        private string path2; // 用于保存路径2
+        
+        public Form1()
+        {
+            InitializeComponent();
+            // 初始化路径
+            path1 = "F:/信息";
+            path2 = "";
+
+            // 根据path1加载TreeView
+            LoadTreeView(path1);
+        }
+        private void LoadTreeView(string path)
+        {
+
+            // 创建一个节点，用于表示当前路径
+            TreeNode rootNode = new TreeNode(Path.GetFileName(path));
+            rootNode.Tag = path;  // 把完整路径保存在Tag属性中，以便后续使用
+            treeView1.Nodes.Add(rootNode);
+
+            // 添加文件夹和文件到TreeView
+            AddDirectoriesAndFilesToTreeView(rootNode);
+
+            // 展开TreeView的根节点
+            rootNode.Expand();
+        }
+
+        private void AddDirectoriesAndFilesToTreeView(TreeNode parentNode)
+        {
+            // 获取parentNode所表示的路径
+            string path = (string)parentNode.Tag;
+
+            // 获取该路径下的所有目录，并为每个目录创建一个节点，添加到parentNode下
+            string[] directories = Directory.GetDirectories(path);
+            foreach (string directory in directories)
+            {
+                TreeNode directoryNode = new TreeNode(Path.GetFileName(directory));
+                directoryNode.Tag = directory;  // 把完整路径保存在Tag属性中，以便后续使用
+                parentNode.Nodes.Add(directoryNode);
+
+                // 递归地添加该目录下的目录和文件
+                AddDirectoriesAndFilesToTreeView(directoryNode);
+            }
+
+            // 获取该路径下的所有文件，并为每个文件创建一个节点，添加到parentNode下
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                TreeNode fileNode = new TreeNode(Path.GetFileName(file));
+                fileNode.Tag = file;  // 把完整路径保存在Tag属性中，以便后续使用
+                parentNode.Nodes.Add(fileNode);
+
+                // 如果文件是"提示词.txt"，则读取文件内容并创建子节点
+                if (Path.GetFileName(file) == "提示词.txt")
+                {
+                    string[] lines = File.ReadAllLines(file);
+                    foreach (string line in lines)
+                    {
+                        TreeNode lineNode = new TreeNode(line);
+                        fileNode.Nodes.Add(lineNode);
+                    }
+                }
+            }
+        }
+
+
+
+        private void treeView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // 获取鼠标位置下的节点
+            TreeNode node = treeView1.GetNodeAt(e.X, e.Y);
+
+            if (node != null)
+            {
+                // 选中这个节点
+                treeView1.SelectedNode = node;
+
+                // 检测是否为左键点击
+                if (e.Button == MouseButtons.Left)
+                {
+                    // 获取鼠标位置下的节点
+                    TreeNode selectedNode = treeView1.GetNodeAt(e.X, e.Y);
+                    if (selectedNode != null)
+                    {
+                        // 选中这个节点
+                        treeView1.SelectedNode = selectedNode;
+
+                        // 检查选中的节点是否是"提示词.txt"
+                        if (selectedNode.Text == "提示词.txt")
+                        {
+                            // 获取节点的完整路径，并移除根节点部分
+                            string relativePath = selectedNode.FullPath.Substring(treeView1.Nodes[0].Text.Length + 1);  // +1 是因为还有一个分隔符
+
+                            // 更新路径2为"提示词.txt"所在的文件夹路径
+                            path2 = Path.Combine(path1, relativePath);
+                            path2 = Path.GetDirectoryName(path2);
+                        }
+                    }
+                }
+                else if (e.Button == MouseButtons.Right)
+                {
+                    // 获取节点的文本内容
+                    string nodeText = node.Text;
+
+                    // 如果文本内容存在以“----”的分隔符
+                    if (nodeText.Contains("----"))
+                    {
+                        string[] parts = nodeText.Split(new string[] { "----" }, StringSplitOptions.None);
+
+                        if (checkBox2.Checked)
+                        {
+                            // 如果选择框2选中，复制中间的内容
+                            Clipboard.SetText(parts[1]);
+                        }
+                        else
+                        {
+                            // 如果选择框2未选中，复制右边的内容
+                            Clipboard.SetText(parts[2]);
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)  // 这里 checkBox1 是选择框1的名称
+            {
+                string contentToWrite = Environment.NewLine + textBox1.Text + "----" + textBox2.Text + "----" + textBox3.Text;  // 这里 textBox1, textBox2, textBox3 分别是文本框1,2,3的名称
+
+                File.AppendAllText(path2 + @"\提示词.txt", contentToWrite, Encoding.UTF8);
+            }
+            else
+            {
+                MessageBox.Show("错误：没有进入编辑模式");
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 清空文本框1的内容
+            textBox1.Text = "";
+
+            // 清空文本框2的内容
+            textBox2.Text = "";
+
+            // 清空文本框3的内容
+            textBox3.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // 检查文件是否存在
+            if (File.Exists(Path.Combine(path2, "提示词.txt")))
+            {
+                // 使用系统默认的文本编辑器打开文件
+                System.Diagnostics.Process.Start(Path.Combine(path2, "提示词.txt"));
+            }
+            else
+            {
+                MessageBox.Show("文件不存在");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();  // 首先清除旧的节点
+            LoadTreeView(path1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenPathInExplorer(path2);
+        }
+
+        private void OpenPathInExplorer(string path)
+        {
+            try
+            {
+                Process.Start("explorer.exe", path);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"无法打开路径: {path}\n错误详情: {ex.Message}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // 获取textBox1的内容作为文件夹名称
+            string folderName = textBox1.Text.Trim();
+            if (string.IsNullOrEmpty(folderName))
+            {
+                MessageBox.Show("文件夹名称不能为空。");
+                return;
+            }
+
+            // 定义目标路径
+            string fullPath = Path.Combine(path2, folderName);
+
+            // 检查文件夹是否存在
+            if (Directory.Exists(fullPath))
+            {
+                MessageBox.Show("该文件夹已存在。");
+            }
+            else
+            {
+                try
+                {
+                    // 创建文件夹
+                    Directory.CreateDirectory(fullPath);
+                    // 定义目标路径
+                    
+                    // 在新建的文件夹中创建文本文件“提示词.txt”
+                    string filePath = Path.Combine(fullPath, "提示词.txt");
+                    // 使用File.Create来创建文件后，需要关闭该文件的句柄，否则可能会导致后续操作该文件时出现异常
+                    using (var stream = File.Create(filePath)) { }
+                    MessageBox.Show($"文件夹 '{folderName}' 和文件 '提示词.txt' 创建成功。");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"发生错误: {ex.Message}");
+                }
+            }
+        }
+    }
+}
